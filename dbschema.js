d53e6758bc838aa48aa5651d6d2f768077f69118
Carlos Cangiano <carlos.cangiano@ross.com.ar>
2021-05-29 01:56:21 -0300

    Add and get user profile details

---
+++ dbschema.js
@@ -0,0 +1,47 @@
+let db = {
+    users: [
+        {
+            userId: 'gRcJreLvOvVNph6NRdbPAYBx00a2',
+            email: 'tom7@gmail.com',
+            handle: 'tom7',
+            createdAt: '2021-05-27T01:49:14.867Z',
+            imageUrl: 'https://firebasestorage.googleapis.com/v0/b/cangis-98db2.appspot.com/o/97787043995.png?alt=media',
+            bio: 'User JCC',
+            website: 'juancarloscangiano.com',
+            location: 'London, UK'
+        }
+    ],
+    screams: [
+        {
+            userHandle: 'Tom3',
+            body: 'tom4@gmail.com',
+            createdAt: '2021-05-21T20:58:08.971Z',
+            likeCount: 5,
+            commentCount: 3
+        }
+    ]
+};
+
+const userDetails = {
+    // Redux data
+    credential: {
+        userId: "gRcJreLvOvVNph6NRdbPAYBx00a2",
+        email: "tom7@gmail.com",
+        handle: "tom7",
+        createdAt: "2021-05-27T01:49:14.867Z",
+        imageUrl: "https://google.com",
+        bio: "hello bio",
+        website: "http://cangis.com",
+        location: "CBA, AR"
+    },
+    likes: [
+        {
+            userHandle: 'user',
+            screamId: 'klajsdklj1k3j423jfjwekj'
+        },
+        {
+            userHandle: 'user',
+            screamId: 'klajsdklj1kokñkopilñwej'
+        }
+    ]
+};
