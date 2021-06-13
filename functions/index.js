5d5c677d4bdbc19f29021799474f02b5b0cc28b3
Carlos Cangiano <carlos.cangiano@ross.com.ar>
2021-06-12 23:58:52 -0300

    Like, Unlike and Delete Post

--- functions/index.js
+++ functions/index.js
@@ -2,24 +2,38 @@ const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/FBAuth');
 
-const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams');
-const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser} = require('./handlers/users');  
+const { 
+    getAllScreams,
+    postOneScream,
+    getScream,
+    commentOnScream,
+    likeScream,
+    unlikeScream,
+    deleteScream
+} = require('./handlers/screams');
+
+const {
+    signup,
+    login,
+    uploadImage,
+    addUserDetails,
+    getAuthenticatedUser
+} = require('./handlers/users');  
 
 // Scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
-// TODO: Delete scream
-// TODO: Like a scream
-// TODO: Unlike a scream
-// TODO: Comment on scream
+app.delete('/scream/:screamId', FBAuth, deleteScream);
+app.get('/scream/:screamId/like', FBAuth, likeScream);
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 // Users routes
 app.post('/signup', signup);
 app.post('/login', login);
-app.post('/user/image', FBAuth, uploadImage);
-app.post('/user', FBAuth, addUserDetails);
 app.get('/user', FBAuth, getAuthenticatedUser);
+app.post('/user', FBAuth, addUserDetails);
+app.post('/user/image', FBAuth, uploadImage);
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
